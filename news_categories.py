5cfec89468aa19372154dc1a67017170731af63d
Ali Moosavi <amoosavi41@gmail.com>
2025-03-30 13:16:31 +0330

    Feat: Add news categories

---
+++ news_categories.py
@@ -0,0 +1,15 @@
+NEWS_CATEGORIES = [
+    "Politics",
+    "Economy & Finance",
+    "Technology",
+    "Science & Health",
+    "Sports",
+    "Entertainment",
+    "World News",
+    "Crime & Law",
+    "Environment",
+    "Education",
+    "Lifestyle",
+    "Science & Innovation",
+    "Local News"
+]
